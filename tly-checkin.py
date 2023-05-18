f59b7ac55f50db6eb17be21a163b94d9814279cf
tianyagogogo <24786545+tianyagogogo@users.noreply.github.com>
2023-05-18 08:22:10 +0800

    Update tly-checkin.py

--- tly-checkin.py
+++ tly-checkin.py
@@ -13,10 +13,12 @@ import json
 import os
 from datetime import datetime
 import cloudscraper
-
+from notify import send
 
 cookie = os.environ["tly_cookie"]
 token = os.environ["bhshare_token"]
+userid = os.environ["userid"]
+apikey = os.environ["apikey"]
 
 scraper = cloudscraper.create_scraper()
 
@@ -38,6 +40,25 @@ def imgcode_online(imgurl):
     else:
         print(result['msg'])
         return 'error'
+
+
+def apitruecaptcha(content : BinaryIO) -> str:
+    image=base64.b64encode(content)
+    url='https://api.apitruecaptcha.org/one/gettext'
+    data={
+        'data':str(image,'utf-8'),
+        'userid':userid,
+        'apikey':apikey
+    }
+    result = requests.post(url, json.dumps(data), proxies = proxies)
+    res=result.json()
+    try:
+        code = res['result']
+    except:
+        log.debug(f"api error,{str(res)}")
+        code = "XXXX"
+    log.debug("apitruecaptcha code: %s" % code)
+    return code
 
 
 def getmidstring(html, start_str, end):
@@ -68,12 +89,15 @@ def tly():
         signurl="https://tly.com/modules/_checkin.php?captcha="
         hearder={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36','Cookie':cookie}
         res1=scraper.get(url=captchaUrl,headers=hearder)
-        base64_data = base64.b64encode(res1.content)
-        oocr=imgcode_online('data:image/jpeg;base64,'+str(base64_data, 'utf-8'))
+        #base64_data = base64.b64encode(res1.content)
+        #oocr=imgcode_online('data:image/jpeg;base64,'+str(base64_data, 'utf-8'))
+        oocr=apitruecaptcha(res1.content)
         res2=scraper.get(url=signurl+oocr.upper(),headers=hearder).text
         print(res2)
+        send('tly签到：', res2)
     else:
         print("还未到时间！",t-timeStamp)
+        send('tly签到：', "还未到时间！" + str(t-timeStamp))
 
 
 def main_handler(event, context):
