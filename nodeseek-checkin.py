26fd56edff9b85dea2051954f9cc300823e2a4a0
lzt <1@1.com>
2023-06-18 02:27:50 +0000

    tj

--- nodeseek-checkin.py
+++ nodeseek-checkin.py
@@ -17,11 +17,17 @@ from typing import BinaryIO , Dict , List , Union
 
 local_cookies = os.environ["nodeseek_cookies"]
 
+
 def checkin():
-    url="https://www.nodeseek.com/api/attendance?random=false";
-    hearder={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36','Cookie':local_cookies}
-    res = requests.post(url=url, headers=hearder, verify=False).text
-    print(res)
+    Cookies = local_cookies.split('&')
+    #print(Cookies)
+    for Cookie in  Cookies:
+        print('-----------------------')
+        print(Cookie)
+        url="https://www.nodeseek.com/api/attendance?random=false";
+        hearder={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36','Cookie':Cookie}
+        res = requests.post(url=url, headers=hearder, verify=False).text
+        print(res)
 
 if __name__ == '__main__':
     checkin()
